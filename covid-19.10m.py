91fbdfa62c8e3eec928b183ecf425b6873ea032f
milo <milo@redbebal.com>
2020-03-09 10:49:43 +0100

    first

--- covid-19.10m.py
+++ covid-19.10m.py
@@ -5,22 +5,33 @@ import urllib.request
 import urllib.parse
 import urllib.error
 import json
+import locale
+
+locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')  # Use '' for auto, or force e.g. to 'en_US.UTF-8'
+
 
 def getValue(result, countryCode):
-    return list(filter(lambda x : x['country_code']==countryCode,result['confirmed']['locations']))
+    return list(filter(lambda x: x['country_code'] == countryCode, result['confirmed']['locations']))[0]
+
 
 url = 'https://coronavirus-tracker-api.herokuapp.com/all'
 response = urllib.request.urlopen(url)
 result = json.loads(response.read())
-str='\u271D {%f}| color=red () | color=green'.format(result['latest']['confirmed'])
-print(str)
+
+strWrld = '\u271D {:n}| color=red'.format(
+    result['latest']['confirmed']
+)
+print(strWrld)
 print("---")
 
-strIT='IT \u271D {%f}| color=red () | color=green'.format(getValue(result, 'IT')['latest']) 
+strIT = 'IT \u271D {:n}| color=red'.format(
+    getValue(result, 'IT')['latest']
+)
 print(strIT)
 print("---")
 
-strNL='NL \u271D {}| color=red () | color=green'.format(getValue(result, 'NL')['latest']) 
+strNL = 'NL \u271D {:n}| color=red'.format(
+    getValue(result, 'NL')['latest']
+)
 print(strNL)
 print("---")
-
